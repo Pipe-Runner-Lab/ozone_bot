f32b0da365b50d23fb0c3f2803f474100601184a
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-12-03 13:52:36 +0530

    README added

--- security_command.py
+++ security_command.py
@@ -1,6 +1,9 @@
 # importing command handler from telegram.ext
 from telegram.ext import (CommandHandler)
 
+# importing password
+from password import (CODE)
+
 # importing the subprocess module
 from subprocess import (call)
 
@@ -8,24 +11,34 @@ from subprocess import (call)
 from datetime import datetime
 
 
-def send_screenshot(bot, update):
-    call(["import", "-window", "root", "./screenshot/temp.jpg"])
-    photo = open("./screenshot/temp.jpg", 'rb')
-    bot.send_photo(chat_id=update.message.chat_id,
-                   caption=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                   photo=photo)
-    call(['rm', '-rf', './screenshot/temp.jpg'])
+def send_screenshot(bot, update, args):
+    if len(args) == 0 or args[0] != CODE:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="You are not authorized to use this command")
+    else:
+        call(["import", "-window", "root", "./screenshot/temp.jpg"])
+        photo = open("./screenshot/temp.jpg", 'rb')
+        bot.send_photo(chat_id=update.message.chat_id,
+                       caption=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                       photo=photo)
+        call(['rm', '-rf', './screenshot/temp.jpg'])
 
 
-def send_camerashot(bot, update):
-    call(['fswebcam', '-r', '1600x1200', '--jpeg', '-1', '-D', '0',
-          '-S', '15', '--no-banner', './screenshot/camera_temp.jpg'])
-    photo = open("./screenshot/camera_temp.jpg", 'rb')
-    bot.send_photo(chat_id=update.message.chat_id,
-                   caption=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                   photo=photo)
-    call(['rm', '-rf', './screenshot/camera_temp.jpg'])
+def send_camerashot(bot, update, args):
+    if len(args) == 0 or args[0] != CODE:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="You are not authorized to use this command")
+    else:
+        call(['fswebcam', '-r', '1600x1200', '--jpeg', '-1', '-D', '0',
+              '-S', '15', '--no-banner', './screenshot/camera_temp.jpg'])
+        photo = open("./screenshot/camera_temp.jpg", 'rb')
+        bot.send_photo(chat_id=update.message.chat_id,
+                       caption=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                       photo=photo)
+        call(['rm', '-rf', './screenshot/camera_temp.jpg'])
 
 
-SEND_SCREENSHOT_HANDLER = CommandHandler('screenshot', send_screenshot)
-SEND_CAMERASHOT_HANDLER = CommandHandler('camerashot', send_camerashot)
+SEND_SCREENSHOT_HANDLER = CommandHandler(
+    'screenshot', send_screenshot, pass_args=True)
+SEND_CAMERASHOT_HANDLER = CommandHandler(
+    'camerashot', send_camerashot, pass_args=True)
